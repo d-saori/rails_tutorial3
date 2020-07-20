f83df170d335cfce4a41ca0308b81179b09c99d6
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-20 23:28:57 +0900

    aboutアクション,ビュー追加, テストコード追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # Homeページのテスト
   test "should get home" do
+    # GETリクエストをhomeアクションに対して発行 (=送信) せよ
     get static_pages_home_url
+    # そうすれば、リクエストに対するレスポンスは[成功]になるはず
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
