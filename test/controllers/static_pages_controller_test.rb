baf4590517ced016b30e3544f137f3a789e0efec
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-20 23:36:46 +0900

    テストコード追加, ビューページ修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,16 +7,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     # そうすれば、リクエストに対するレスポンスは[成功]になるはず
     assert_response :success
+    # <title>タグ内に『Home | Ruby on Rails Tutorial Sample App』という文字列があるかどうかをチェック
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
