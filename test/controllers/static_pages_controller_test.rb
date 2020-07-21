872f7bfd976a50e991844cb467b448cf85243b04
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-21 18:47:52 +0900

    テストコード、ビューコード修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   # 各テストが実行される直前で実行されるメソッド
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
+  # def setup
+  #   @base_title = "Ruby on Rails Tutorial Sample App"
+  # end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
+  # test "should get root" do
+  #   get root_url
+  #   assert_response :success
+  # end
 
   # Homeページのテスト
   test "should get home" do
@@ -20,26 +20,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     # <title>タグ内に『Home | Ruby on Rails Tutorial Sample App』という文字列があるかどうかをチェック
     # assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "Home | #{@base_title}"
+    # assert_select "title", "Home | #{@base_title}"
+    # full_titleヘルパーを定義したので、Homeページにこれまで表示されていた余分な「Home」という単語を表示せず、基本タイトルのみを正しく表示する事もできるようになった。"Home"という文字を表示されていないことを確認するテスト。
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     # assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "Help | #{@base_title}"
+    # assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
     # assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "About | #{@base_title}"
+    # assert_select "title", "About | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    # assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 end
