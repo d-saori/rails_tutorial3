11f95d134c2965f98ccf0c4516b6f9da1d37e40f
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-30 17:54:59 +0900

    有効なユーザー登録に対するテスト, flashをテストするためのテンプレート追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,6 @@ module UsersHelper
   # キーワード引数使用
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
