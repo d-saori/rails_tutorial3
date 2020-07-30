55d05b9aaff1f5a1de7c34883865737ed872f223
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-31 00:04:26 +0900

    フラッシュメッセージ実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
+      flash.now[:danger] = 'lnvalid email/password combination'
       render 'new'
     end
   end
