f2ab84bc03cc3a94e562200c078426967830272b
d-saori <60836449+d-saori@users.noreply.github.com>
2020-08-02 23:53:13 +0900

    コード修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'lnvalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
