cbef95333819e6725378161de9164b0085389ff1
d-saori <60836449+d-saori@users.noreply.github.com>
2020-08-11 21:59:15 +0900

    beforeフィルターを使って編集/更新ページを保護する

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,7 +25,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "should redirect when logged in as wrong user" do
+  test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name
+    patch user_path(@user), params: { user: { name: @user.name,
                             email: @user.email }}
     assert flash.empty?
     assert_redirected_to root_url
