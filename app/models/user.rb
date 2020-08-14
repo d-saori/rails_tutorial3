c23c1781174e0b6f16794292f9e307112f2e4e63
d-saori <60836449+d-saori@users.noreply.github.com>
2020-08-14 23:42:08 +0900

    downcase_emailメ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_tpken
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -43,7 +43,7 @@ class User < ApplicationRecord
 
   # メールアドレスをすべて小文字にする
   def downcase_email
-    self.email = email.downcase
+    email.downcase!
   end
 
   # 有効化トークンとダイジェストを作成および代入する
