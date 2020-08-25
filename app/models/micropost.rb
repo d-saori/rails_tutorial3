a64e9cb6ea84f0f832f06a7f6a14547f008aeaed
d-saori <60836449+d-saori@users.noreply.github.com>
2020-08-26 00:21:33 +0900

    マイクロポストは、その所有者 (ユーザー) と一緒に破棄されることを保証する

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
