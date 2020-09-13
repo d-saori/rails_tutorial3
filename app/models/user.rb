ab2d3e8e9abc30302f367fdd04e76dc6f8199abf
d-saori <60836449+d-saori@users.noreply.github.com>
2020-09-13 23:30:26 +0900

    受動的関係を使ってuser.followersを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                     foreign_key: "follower_id",
                     dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                    foreign_key: "followed_id",
+                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -90,7 +94,7 @@ class User < ApplicationRecord
 
   # ユーザーをフォロー解除する
   def unfollow(other_user)
-    active_relationships.find_by(follower_id: other_user.id).destroy
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   # 現在のユーザーがフォローしてたらtrueを返す
