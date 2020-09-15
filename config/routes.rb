f0ce54e07170c6f4b3651cc3e86c94c23f116804
d-saori <60836449+d-saori@users.noreply.github.com>
2020-09-15 23:43:16 +0900

    Usersコントローラにfollowingアクションとfollowersアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   # get 'password_resets/new'
   # get 'password_resets/edit'
   # post '/signup', to: 'users#create'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only:[:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
   resources :microposts,    only:[:create, :destroy]
